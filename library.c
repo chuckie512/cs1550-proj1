9a919211ab14d83ce5b910332fc7b8ad301ef0b0
Charles Smith <cas275@pitt.edu>
2015-09-27 17:55:10 -0400

    its done now

--- library.c
+++ library.c
@@ -6,35 +6,167 @@
  */
 
 #include<time.h>
+#include<linux/fb.h>
+#include<sys/ioctl.h>
+#include<termios.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/mman.h>
+#include<stdint.h>
+#include"iso_font.h"
+
+typedef uint16_t color_t;
+
+void * graphics_map;
+int graphics_fd;
+struct fb_fix_screeninfo fixed;
+struct fb_var_screeninfo var;
+struct termios term;
 
 void init_graphics(){
-    //TODO
+
+    /* get the file discripter for the framebuffer*/
+    graphics_fd = open("/dev/fb0", O_RDWR);
+
+    /*find out what kind of screen we're using*/
+    ioctl(graphics_fd, FBIOGET_VSCREENINFO, &var);
+    ioctl(graphics_fd, FBIOGET_FSCREENINFO, &fixed);
+
+    /*find out how big that screen really is*/
+    int mmsize = fixed.line_length * var.yres;
+
+    /*mmap the frame buffer*/
+    graphics_map = mmap(NULL, mmsize , PROT_READ | PROT_WRITE,
+      MAP_SHARED, graphics_fd, 0  );
+
+    /*now to deal with that pesky terminal*/
+    ioctl(0,TCGETS,&term);
+    struct termios new_term;
+    new_term = term;
+    new_term.c_lflag&=~ECHO;
+    new_term.c_lflag&=~ICANON;
+    ioctl(0,TCSETS,&new_term);
+    /*
+     * I thought here would be a good place to call clear
+     * but the TA said otherwise
+     */
 }
 
 void exit_graphics(){
-    //TODO
+    /*Let's leave things how we found them*/
+    munmap(graphics_map, 0); //unmap the mmap
+    close(graphics_fd);      //close the fd
+
+    /* and fix that terminal */
+    ioctl(0,TCSETS, &term);
 }
 
 void clear_screen(){
-    //this works, but is 4 right?
-    write(1, "\033[2J",4);
+    //this works, but is 7 right?
+    write(1, "\033[2J",7);
 }
 
 char getkey(){
-    //TODO
+    /* get key will return null if nothing is ready to read */
+    struct timeval time = { 0L, 0L};
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(0, &fds);
+    int val=0;
+    val=select(1, &fds, NULL, NULL, &time);
+
+    if(val>0){ /* we can read */
+        char input;
+        read(0, &input, sizeof(input));
+        return input;
+    }
+    else{ /* not ready yet */
+        return NULL;
+    }
+
+
 }
 
 void sleep_ms(long ms){
-    //TODO
     struct timespec time;
     time.tv_sec=ms/1000;
-    time.tv_nsec=ms%1000;
+    time.tv_nsec=(ms%1000)*1000000;
     nanosleep(&time, NULL);
 }
 
-/*
 void draw_pixel(int x, int y, color_t color){
-    //TODO
-}
-*/
 
+    int loc = x * fixed.line_length + y*2;
+    color_t* pixel = (graphics_map + loc);
+    *pixel = color;
+
+}
+
+void draw_rect(int x, int y, int width, int height, color_t c){
+    int i;
+    for(i=x; i<x+width; i++){
+        draw_pixel(y, i, c);
+        draw_pixel(y+height, i, c);
+    }
+
+    for(i=y; i<=y+height; i++){
+        draw_pixel(i, x, c);
+        draw_pixel(i, x+width, c);
+    }
+}
+
+void draw_text(int x, int y, const char* text, color_t c){
+    //TODO
+
+    int n=0;
+    char t = text[n];
+    while(t!='\0'){ /* iterate over the whole string */
+
+    int i;
+    for(i=0; i<16; i++){
+        unsigned char byte = iso_font[ 16 * text[n] + i];
+        int j;
+        int k=0;
+        for(j=1; j<=128; j*=2){
+            if(byte&j)
+                draw_pixel(x+i, y+k, c);
+            k++;
+        }
+    }
+    t = text[++n];
+    y+=8;
+    }
+}
+
+void fill_circle(int x0, int y0, int r, color_t c){
+
+    
+    while(r>0){  /* keep making smaller circles, until its full */
+    int x = r;
+    int y = 0;
+    int decision_over_2 = 1-x;
+    
+    while(y<=x){
+        draw_pixel(  x+x0,  y+y0, c);
+        draw_pixel(  y+x0,  x+y0, c);
+        draw_pixel( -x+x0,  y+y0, c);
+        draw_pixel( -y+x0,  x+y0, c);
+        draw_pixel( -x+x0, -y+y0, c);
+        draw_pixel( -y+x0, -x+y0, c);
+        draw_pixel(  x+x0, -y+y0, c);
+        draw_pixel(  y+x0, -x+y0, c);
+        y++;
+        if(decision_over_2<=0){
+            decision_over_2 += 2 * y + 1;
+        }
+        else{
+            x--;
+            decision_over_2 += 2 * (y-x) + 1;
+        }
+    }
+    r--;
+    }
+    if(r==0){ /* if there is no radius it's just a point */
+        draw_pixel(x0,y0,c);
+    }
+}
--- test.c
+++ test.c
@@ -1,9 +1,65 @@
 #include<stdio.h>
 #include"library.c"
-
+#define BLUE  30
+#define GREEN 2016
+#define RED   63488
 int main(void){
-printf("string\n");
+printf("start\n");
 sleep_ms(1500);
 clear_screen();
+
+init_graphics();
+int i=0;
+for( i; i<480; i++){
+    draw_pixel(i, 50, BLUE );
+    draw_pixel(i, 60, GREEN);
+    draw_pixel(i, 70, RED  );
+}
+sleep_ms(100);
+for(i=0; i<640; i++){
+    draw_pixel(50, i, RED  );
+    draw_pixel(60, i, GREEN);
+    draw_pixel(70, i, BLUE );
+}
+sleep_ms(250);
+
+draw_rect(100,100,30,30, RED  );
+sleep_ms(250);
+draw_rect(110,110,10,10, GREEN);
+sleep_ms(250);
+draw_rect( 90, 90,50,50, BLUE );
+
+sleep_ms(1000);
+
+fill_circle(200,200, 50, BLUE);
+
+draw_rect(150,150,100,100,RED);
+
+sleep_ms(250);
+fill_circle(200,200,20,GREEN);
+
+sleep_ms(250);
+fill_circle(400,200,10, GREEN);
+
+clear_screen();
+int q=0;
+int z=0;
+while(getkey()==NULL&&q<600){
+    for(q=0; q<480; q++){
+        draw_pixel(q,z,GREEN);
+    }
+    z++;
+    sleep_ms(100);
+}
+clear_screen();
+
+draw_text(10,10, "Testing writing text", RED);
+draw_text(50,10, "This is BLUE", BLUE);
+sleep_ms(250);
+exit_graphics();
+
+printf("done\n");
+
 return 0;
+
 }
